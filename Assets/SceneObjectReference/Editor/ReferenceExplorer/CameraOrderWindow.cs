8a53678f62c21d0d56c0c5492c90ec6511fc6948
山村 達彦 <ruitomo.ex@gmail.com>
2014-12-03 02:11:10 +0900

    Add Renderer Order

--- Assets/SceneObjectReference/Editor/ReferenceExplorer/CameraOrderWindow.cs
+++ Assets/SceneObjectReference/Editor/ReferenceExplorer/CameraOrderWindow.cs
@@ -3,20 +3,31 @@ using System.Collections;
 using UnityEditor;
 using System.Collections.Generic;
 
+#pragma warning disable 0618
+
 namespace ReferenceExplorer
 {
 	public class CameraOrderWindow : EditorWindow {
 
-		//[MenuItem("Window/Referenced/Camera Order")]
+		public enum OrderType
+		{
+			CameraOrder,
+			SpriteOrder
+		}
+
+		public OrderType orderType;
+
+		[MenuItem("Window/ReferenceExplorer/Renderer Order")]
 		static void Init()
 		{
-			var window = CameraOrderWindow.GetWindow<CameraOrderWindow>("camera order");
+			var window = CameraOrderWindow.GetWindow<CameraOrderWindow>("order");
 			window.Show();
 		}
 
 		void OnFocus()
 		{
 			UpdateCameras();
+			SortLayers();
 		}
 
 		void OnHierarchyChange()
@@ -43,6 +54,7 @@ namespace ReferenceExplorer
 			}
 
 			SortCamera();
+
 		}
 
 		void SortCamera()
@@ -50,28 +62,123 @@ namespace ReferenceExplorer
 			allCameras.Sort( (x, y) => {	return (int)(1000 * x.depth) - (int)(1000 * y.depth); });
 		}
 
+
+		void SortLayers()
+		{
+			layers.Clear();
+			SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+			var sortinglayer = tagManager.FindProperty("m_SortingLayers");
+
+			for(int i=0; i<sortinglayer.arraySize; i++)
+			{
+				var tag = sortinglayer.GetArrayElementAtIndex(i);
+				layers.Add( new SortingLayerWithObject(){ tagName = tag.displayName });
+			}
+			SceneObjectUtility.CollectionAllComponent();
+			foreach( var renderer in FindObjectsOfType<SpriteRenderer>())
+			{
+				if( renderer.gameObject.name.IndexOf(saerchText) == -1 )
+					continue;
+
+				var sortingLayerName =  string.IsNullOrEmpty( renderer.sortingLayerName ) ? "Default" : renderer.sortingLayerName ;
+				var tag = layers.Find( t => t.tagName == sortingLayerName);
+
+				Debug.Log(renderer.gameObject.name + "/" + renderer.sortingLayerName );
+
+				tag.rendererList.Add( renderer);
+			}
+
+			foreach( var layer in layers )
+			{
+				layer.rendererList.Sort( (x,y) =>  x.sortingOrder - y.sortingOrder);
+			}
+		}
+
+		List<SortingLayerWithObject> layers = new List<SortingLayerWithObject>();
 		List<Camera> allCameras = new List<Camera>();
+
+		string saerchText = string.Empty;
 		Vector2 current;
 
-		void OnGUI()
+		void OnGUISpriteOrder()
+		{
+			EditorGUILayout.BeginHorizontal("box");
+			
+			EditorGUI.BeginChangeCheck();
+			EditorGUILayout.LabelField("Search", GUILayout.Width(60));
+			saerchText = EditorGUILayout.TextField(saerchText);
+			if( EditorGUI.EndChangeCheck())
+			{
+				SortLayers();
+			}
+			
+			EditorGUILayout.EndHorizontal();
+			
+			current = EditorGUILayout.BeginScrollView(current);
+			
+			
+			
+			foreach( var layer in layers )
+			{
+				EditorGUI.indentLevel = 0;
+				layer.isOpen = EditorGUILayout.Foldout (layer.isOpen, layer.tagName); 
+				if( layer.isOpen )
+				{
+					EditorGUI.indentLevel = 1;
+					foreach( var obj in layer.rendererList )
+					{
+						EditorGUILayout.BeginHorizontal();
+						obj.sortingOrder = EditorGUILayout.IntField(obj.sortingOrder, GUILayout.Width(40));
+						EditorGUILayout.ObjectField(obj, typeof(SpriteRenderer), true);
+						EditorGUILayout.EndHorizontal();
+					}
+				}
+				EditorGUI.indentLevel = 0;
+			}
+			EditorGUILayout.EndScrollView();
+		}
+
+		void OnGUICameraOrder()
 		{
 			current = EditorGUILayout.BeginScrollView(current);
-
-			EditorGUILayout.BeginHorizontal("box");
-			EditorGUILayout.LabelField("camera order");
-			EditorGUILayout.EndHorizontal();
 
 			EditorGUILayout.BeginVertical("box");
 			foreach( var camera in allCameras )
 			{
 				EditorGUILayout.BeginHorizontal();
-				EditorGUILayout.LabelField( camera.depth.ToString(), GUILayout.MaxWidth(50), GUILayout.MinWidth(20));
+				camera.depth = EditorGUILayout.FloatField(camera.depth, GUILayout.Width(40));
 				EditorGUILayout.ObjectField(camera.gameObject, typeof(GameObject) );
 				EditorGUILayout.EndHorizontal();
 			}
 			EditorGUILayout.EndVertical();
 
 			EditorGUILayout.EndScrollView();
+
+		}
+
+		void OnGUI()
+		{
+		 	orderType = (OrderType) EditorGUILayout.EnumPopup(orderType, EditorStyles.toolbarPopup);
+
+			switch( orderType )
+			{
+			case OrderType.SpriteOrder:
+				OnGUISpriteOrder();
+				break;
+			case OrderType.CameraOrder:
+				OnGUICameraOrder();
+				break;
+			}
+		}
+
+
+
+		class SortingLayerWithObject
+		{
+			public List<SpriteRenderer> rendererList = new List<SpriteRenderer>();
+			public string tagName;
+			public bool isOpen = true;
 		}
 	}
 }
