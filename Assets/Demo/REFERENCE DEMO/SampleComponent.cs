66f6e333f935bfcc94f6722be2ead7ccf2610674
Yamamura Tatsuhiko <Yamamura Tatsuhiko>
2016-11-18 14:17:17 +0900

    5.5用サンプルプロジェクトの追加

--- Assets/Demo/REFERENCE DEMO/SampleComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SampleComponent : MonoBehaviour 
-{
-
-	public Transform target;
-
-}
---
+++ Assets/Sample/Sample2/DistanceToPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceToPlayer : MonoBehaviour
+{
+	private GameObject player = null;
+
+	[SerializeField]
+	private UnityEngine.UI.Text label = null;
+
+	void Start ()
+	{
+		player = GameObject.FindWithTag ("Player");
+	}
+
+	void Update ()
+	{
+		label.text = string.Format("distance to player : {0:F3}", Vector3.Distance (player.transform.localPosition, transform.localPosition));
+	}
+}
